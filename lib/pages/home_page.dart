54ff3d5051607a8d07f6dbfb2ba5b42f026f38a8
bhaskarkh01 <bhaskarkh01@gmail.com>
2022-10-30 16:14:03 +0530

    add title to home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,13 +6,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 238, 213, 121),
-          image: DecorationImage(
-            fit: BoxFit.contain,
-            image: AssetImage("assets/images/auto.png"),
-          ),
+      body: _pageTitle(),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#LetsGo",
+      style: TextStyle(
+        fontSize: 50,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _autoImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 238, 213, 121),
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/auto.png"),
         ),
       ),
     );
