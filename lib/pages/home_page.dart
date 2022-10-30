04a765ed2a49a8c65a2306ed394135dbcf3df008
bhaskarkh01 <bhaskarkh01@gmail.com>
2022-10-30 16:47:25 +0530

    get size using MediaQuery

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: Container(
+          height: _deviceHeight,
+          width: _deviceWidth,
+          color: Colors.red,
           child: _pageTitle(),
         ),
       ),
