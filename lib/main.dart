1ae909dfc275d1ab41c0d989f7927064ddb441b2
bhaskarkh01 <bhaskarkh01@gmail.com>
2022-10-30 15:08:38 +0530

    set background and MaterialApp

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "LetsRide",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromRGBO(31, 31, 31, 1.0),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
